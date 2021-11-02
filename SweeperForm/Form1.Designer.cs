cc56badc2d41136de2131d61d8c70b76710f5d5a
somelizard <liam.t.mcguire@gmail.com>
2021-11-02 13:40:30 -0400

    Add files via upload

---
+++ SweeperForm/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SweeperForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.easyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mediumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(282, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.easyToolStripMenuItem,
+            this.mediumToolStripMenuItem,
+            this.hardToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 20);
+            this.toolStripMenuItem1.Text = "Game";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // easyToolStripMenuItem
+            // 
+            this.easyToolStripMenuItem.Checked = true;
+            this.easyToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.easyToolStripMenuItem.Text = "Easy";
+            this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
+            // 
+            // mediumToolStripMenuItem
+            // 
+            this.mediumToolStripMenuItem.Name = "mediumToolStripMenuItem";
+            this.mediumToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mediumToolStripMenuItem.Text = "Medium";
+            this.mediumToolStripMenuItem.Click += new System.EventHandler(this.mediumToolStripMenuItem_Click);
+            // 
+            // hardToolStripMenuItem
+            // 
+            this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hardToolStripMenuItem.Text = "Hard";
+            this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 259);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem easyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mediumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hardToolStripMenuItem;
+    }
+}
+
---
+++ SweeperForm/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using SweeperForm.Properties;
+
+using SweeperGame;
+
+using static SweeperGame.Utilities;
+
+
+namespace SweeperForm
+{
+    public partial class Form1 : Form
+    {
+        public Point TopLeft;
+        public Size CellSize = new Size(16, 16);
+        public Game currGame;
+        public List<GameHistory> historyList;
+
+        public PictureBox[,] pictureBoxes;
+
+        public Form1()
+        {
+            InitializeComponent();
+            historyList = new List<GameHistory>();
+            TopLeft = new Point(12, 12 + menuStrip1.Size.Height);
+            InitGame(Game.Easy());
+        }
+
+        public void InitGame(Game game)
+        {
+            this.currGame = game;
+            this.currGame.OnMineHit += OnMineHit;
+            if (pictureBoxes != null)
+            {
+                foreach(var pb in pictureBoxes)
+                {
+                    if (pb.Image != null)
+                    {
+                        pb.Image.Dispose();
+                    }
+
+                    pb.Dispose();
+                }
+            }
+
+            pictureBoxes = new PictureBox[this.currGame.sizeX, this.currGame.sizeY];
+
+            for (int x = 0; x < this.currGame.sizeX; x++)
+            {
+                for (int y = 0; y < this.currGame.sizeY; y++)
+                {
+                    PictureBox newPB = CreateNewCell(x, y);
+                    newPB.Click += OnClickCell;
+                    Controls.Add(newPB);
+                    pictureBoxes[x,y] = newPB;
+                }
+            }
+
+            Log("Made " + pictureBoxes.Length + " new cells");
+
+            int newSizeX = (TopLeft.X * 2) + (CellSize.Width * (this.currGame.sizeX + 1));
+            int newSizeY = (TopLeft.Y * 2) + (CellSize.Height * (this.currGame.sizeY + 1));
+            Log("New width is " + (TopLeft.X * 2) + " + " + CellSize.Width + " * " + this.currGame.sizeX + " = " + newSizeX);
+            Log("New height is " + (TopLeft.Y * 2) + " + " + CellSize.Height + " * " + this.currGame.sizeY + " = " + newSizeY);
+            Size = new Size(newSizeX, newSizeY);
+            Refresh();
+        }
+
+        private void OnClickCell(object sender, EventArgs eventArgs)
+        {
+            string name = ((PictureBox)sender).Name;
+            int x = int.Parse(name.Substring(0, name.IndexOf(',')));
+            int y = int.Parse(name.Substring(name.IndexOf(',') + 1));
+
+            MouseEventArgs e = (MouseEventArgs)eventArgs;
+            if (e.Button == MouseButtons.Left)
+            {
+                currGame.Click(x, y, out ClickResult cr);
+            }
+            else if (e.Button == MouseButtons.Right) {
+                currGame.RightClick(x, y);
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                (List<Cell> mines, List<Cell> safe) = Solver.Solve(currGame.grid);
+                foreach (Cell cell in mines)
+                {
+                    if (!cell.Flagged)
+                    {
+                        currGame.RightClick(cell.x, cell.y);
+                    }
+                    //pictureBoxes[cell.x, cell.y].Image = Resources.Flagged;
+                }
+                foreach(Cell cell in safe) 
+                {
+                    currGame.Click(cell.x, cell.y, out _);
+                    //pictureBoxes[cell.x, cell.y].Image = Resources.Question;
+                }
+                //return;
+            }
+            
+            if (!currGame.Over)
+            {
+                RedrawCells();
+            }
+        }
+
+        public void RedrawCells()
+        {
+            for (int x = 0; x < this.currGame.sizeX; x++)
+            {
+                for (int y = 0; y < this.currGame.sizeY; y++)
+                {
+                    if (currGame.grid[x, y].Flagged)
+                    {
+                        pictureBoxes[x, y].Image = Resources.Flagged;
+                    }
+                    else if (currGame.grid[x, y].Covered)
+                    {
+                        pictureBoxes[x, y].Image = Resources.Covered;
+                    }
+                    else if (!currGame.grid[x, y].Covered && currGame.grid[x, y].MinedNeighbors == 0)
+                    {
+                        pictureBoxes[x, y].Image = Resources.Empty;
+                    }
+                    else if (!currGame.grid[x, y].Covered && currGame.grid[x, y].MinedNeighbors != 0)
+                    {
+                        pictureBoxes[x, y].Image = (Image)Resources.ResourceManager.GetObject("_" + currGame.grid[x, y].MinedNeighbors);
+                    }
+                }
+            }
+        }
+
+        public void OnMineHit(int x, int y)
+        {
+            RevealMines();
+            pictureBoxes[x, y].Image = Resources.MineHit;
+            historyList.Add(currGame.history);
+        }
+
+        public void RevealMines()
+        {
+            for (int x = 0; x < this.currGame.sizeX; x++)
+            {
+                for (int y = 0; y < this.currGame.sizeY; y++)
+                {
+                    if (currGame.grid[x, y].Flagged)
+                    {
+                        if (currGame.grid[x, y].Mined)
+                        {
+                            pictureBoxes[x, y].Image = Resources.MineX;
+                        }
+                        else
+                        {
+                            pictureBoxes[x, y].Image = Resources.Flagged;
+                        }
+                    }
+                    else if (currGame.grid[x, y].Covered && currGame.grid[x, y].Mined)
+                    {
+                        pictureBoxes[x, y].Image = Resources.Mine;
+                    }
+                    else if (!currGame.grid[x, y].Covered && currGame.grid[x, y].MinedNeighbors == 0)
+                    {
+                        pictureBoxes[x, y].Image = Resources.Empty;
+                    }
+                    else if (!currGame.grid[x, y].Covered && currGame.grid[x, y].MinedNeighbors != 0)
+                    {
+                        pictureBoxes[x, y].Image = (Image)Resources.ResourceManager.GetObject("_" + currGame.grid[x, y].MinedNeighbors);
+                    }
+                }
+            }
+        }
+
+        public PictureBox CreateNewCell(int x, int y)
+        {
+            PictureBox newPB = new PictureBox();
+            newPB.BackColor = SystemColors.Control;
+            newPB.Image = Properties.Resources.Covered;
+            newPB.InitialImage = null;
+
+            int locX = TopLeft.X + (x * CellSize.Width);
+            int locY = TopLeft.Y + (y * CellSize.Height);
+
+            newPB.Location = new Point(locX, locY);
+            newPB.Margin = new Padding(0);
+            newPB.Name = x + ", " + y;
+            newPB.Size = CellSize;
+            newPB.TabIndex = 1;
+            newPB.TabStop = false;
+
+            return newPB;
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mediumToolStripMenuItem.Checked = false;
+            hardToolStripMenuItem.Checked = false;
+            InitGame(Game.Easy());
+        }
+
+        private void mediumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            easyToolStripMenuItem.Checked = false;
+            hardToolStripMenuItem.Checked = false;
+            InitGame(Game.Medium());
+        }
+
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            easyToolStripMenuItem.Checked = false;
+            mediumToolStripMenuItem.Checked = false;
+            InitGame(Game.Hard());
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (easyToolStripMenuItem.Checked)
+            {
+                InitGame(Game.Easy());
+            }
+            else if (mediumToolStripMenuItem.Checked)
+            {
+                InitGame(Game.Medium());
+            }
+            else if (hardToolStripMenuItem.Checked)
+            {
+                InitGame(Game.Hard());
+            }
+            else
+            {
+                LogW("None of the form difficulties were selected!");
+            }
+        }
+    }
+}
